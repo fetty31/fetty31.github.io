05ee925180bbcc806a5daf7a351c69a111602c6b
Fetty <fetty3113@gmail.com>
2026-03-31 14:24:53 +0200

    First commit

---
+++ js/data.js
@@ -0,0 +1,46 @@
+const data = {
+    portfolios: [
+        {
+            techStack: "C++, ROS/ROS2",
+            title: "Fast LIMO",
+            githubLink: "https://github.com/fetty31/fast_LIMO",
+            projectLink: "#",
+            imgLink: "images/portfolio/campus_nord.png"
+        },
+        {
+            techStack: "C++",
+            title: "LieOdyssey",
+            githubLink: "https://github.com/fetty31/LieOdyssey",
+            projectLink: "#",
+            imgLink: "images/portfolio/e8petri.png"
+        },
+        {
+            techStack: "C++, MATLAB, ROS",
+            title: "Tailored MPC",
+            githubLink: "https://github.com/fetty31/tailored_mpc",
+            projectLink: "#",
+            imgLink: "images/portfolio/tailored_mpc.png"
+        },
+        {
+            techStack: "C++, IPG CarMaker, ROS",
+            title: "FSDriverless",
+            githubLink: "https://github.com/fetty31/FSDriverless",
+            projectLink: "#",
+            imgLink: "images/portfolio/fsdriverless.png"
+        },
+        {
+            techStack: "C++, ROS",
+            title: "Trajectory Optimizer (TRO)",
+            githubLink: "https://github.com/fetty31/trajectory_optimizer",
+            projectLink: "#",
+            imgLink: "images/portfolio/tro_colorplot.png"
+        },
+        {
+            techStack: "C++, ROS/ROS2",
+            title: "MaPPI",
+            githubLink: "https://github.com/fetty31/mappi",
+            projectLink: "#",
+            imgLink: "images/portfolio/mappi.png"
+        }
+    ]
+}
